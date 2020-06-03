58b1fb11156096aeebf3465090f8d61ddf52da81
V-Perotto <vittorio.perotto@outlook.com>
2020-06-03 13:39:22 -0300

    Pasta [images] adicionada, 3 arquivos de fotos adicionados

---
+++ myservice/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from .views import home
+from accounts.views import login_view, register_view, logout_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("faq", views.faq, name="faq"),
+    path('accounts/login/', login_view, name='login'),
+    path('accounts/register/', register_view, name="register"),
+    path('accounts/logout/', logout_view, name="logout"),
+]
