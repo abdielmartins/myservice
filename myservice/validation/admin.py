c3be27b8f53727d02774d27debb8b04cd435e684
Abdiel Martins <51211403+abdielmartins@users.noreply.github.com>
2020-06-15 17:05:18 -0300

    Revert "Merge pull request #12 from abdielmartins/contribuilding"
    This reverts commit 7dd7f4f220da756a56f77ea9c4e3e6814f0d6772.

--- myservice/validation/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Validation
-
-admin.site.register(Validation)
--- myservice/virtualvenv/Lib/site-packages/crispy_forms_semantic_ui/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SemanticUIConfig(AppConfig):
-    name = 'crispy_forms_semantic_ui'
-    verbose_name = 'Crispy form templates for semantic UI'
-
-    def ready(self):
-        super(SemanticUIConfig, self).ready()
