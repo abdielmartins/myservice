4cc01aa0245558412ed03b02d18d3f6322dfb8be
Abdiel <abdielmartins21@gmail.com>
2020-06-16 17:08:49 -0300

    final version

--- myservice/views.py
+++ myservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from services.models import Services
 
 def home(request):
     return render(request, "home.html", {})
